6fc3d0542cae40e69967c35bfa7ffba4b62a88ee
martinezcyrus <cyrusjaysonm@gmail.com>
2026-05-22 08:54:50 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ''
+          }
+        ]
+      }
+    ]
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
