3c4b1e0be26b86fb7ee88b2db3dd4691053ff904
martinezcyrus <cyrusjaysonm@gmail.com>
2026-05-27 15:40:21 +0800

    Create route.ts

---
+++ app/auth/signout/route.ts
@@ -0,0 +1,15 @@
+import { createClient } from '../../lib/server'
+import { NextResponse } from 'next/server'
+
+export async function POST(request: Request) {
+  const supabase = createClient()
+
+  const { data: { user } } = await supabase.auth.getUser()
+
+  if (user) {
+    await supabase.auth.signOut()
+  }
+
+  const { origin } = new URL(request.url)
+  return NextResponse.redirect(`${origin}/login`, { status: 302 })
+}
