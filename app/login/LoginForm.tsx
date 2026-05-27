08396a4f29f871eb0a8ddd2d16857a86cb8737e1
martinezcyrus <cyrusjaysonm@gmail.com>
2026-05-27 17:08:57 +0800

    Update LoginForm.tsx

--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { createClient } from '../../lib/client'
+import { createClient } from '../lib/client'
 
 interface LoginFormProps {
   redirectTo?: string
