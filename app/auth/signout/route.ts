02f731115e4fc012cf4dc7833b3c27aa1504db1f
martinezcyrus <cyrusjaysonm@gmail.com>
2026-06-01 17:13:27 +0800

    Update route.ts

--- app/auth/signout/route.ts
+++ app/auth/signout/route.ts
@@ -2,7 +2,7 @@ import { createClient } from '../../lib/server'
 import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
-  const supabase = createClient()
+  const supabase = await createClient()
 
   const { data: { user } } = await supabase.auth.getUser()
 
